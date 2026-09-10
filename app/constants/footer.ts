303ba779bfeabcf1baefab7d7bed8129d2bda723
Saurya Janbandhu <jsaurya101@gmail.com>
2026-09-10 23:08:35 +1200

    feat: add LinkedIn to footer and delete unused 3D model backups

--- app/constants/footer.ts
+++ app/constants/footer.ts
@@ -2,6 +2,12 @@ import { FooterLink } from "../types";
 
 export const FOOTER_LINKS: FooterLink[] = [
   {
+    name: 'LinkedIn',
+    hoverText: 'Connect on LinkedIn',
+    icon: 'icons/linkedin.svg',
+    url: 'https://www.linkedin.com/in/saurya-janbandhu-3771661b0/',
+  },
+  {
     name: 'GitHub',
     hoverText: 'Open-source work',
     icon: 'icons/github.svg',
